9b48beee6faf9e2aa35c3b0b9a65eac3f11d8dd2
Michaela DeCamara <michaela.decamara@gmail.com>
2022-04-01 03:00:26 -0400

    login and register deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 // import Product from "./pages/Product";
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+// import Cart from "./pages/Cart";
 
 const App = () => {
   return <div>
       {/* <Home /> */}
       {/* <ProductList /> */}
       {/* <Product /> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
+      <Login />
+      <Register />
       <Cart />
   </div>
 };
